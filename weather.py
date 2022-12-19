36680705543a79104b99b931cd5ba4ac3c140083
Andrey <a.u.golikov@gmail.com>
2022-12-20 00:08:47 +0300

    create Class Weather and add method to get info

--- weather.py
+++ weather.py
@@ -2,11 +2,31 @@ import requests
 from config import get_openweather_token
 weather_token = get_openweather_token()
 
-def get_weather_for_now():
-    lat = 55.5433
-    lon = 37.5483
-    params = {'units': 'metric',
-            'lang': 'ru'}
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_token}'
-    weather_now = requests.get(url, params=params) 
-    return weather_now.json()
+class Weather():
+
+        def get_weather_for_now(self):
+                self.lat = 55.5433
+                self.lon = 37.5483
+                params = {'units': 'metric',
+                        'lang': 'ru'}
+                url = f'https://api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid={weather_token}'
+                weather_now = requests.get(url, params=params) 
+                return weather_now.json()
+
+        def get_weather_info(self):
+                weather_info_list = []
+                weather_info_list.append(self.get_weather_for_now()['main']['feels_like'])
+                weather_info_list.append(self.get_weather_for_now()['main']['temp'])
+                weather_info_list.append(self.get_weather_for_now()['wind']['speed'])
+                return weather_info_list
+                 
+        def print_info_on_russian(self):
+
+                print(f'Погодка сейчас такая себе: градусов-то всего: {self.get_weather_info()[1]}, но из-за '
+                      f'ебейшего ветра в {self.get_weather_info()[2]} метров в секуду, '
+                      f'ощущается на {self.get_weather_info()[0]}')
+
+
+
+weather = Weather()
+bla = weather.print_info_on_russian()
