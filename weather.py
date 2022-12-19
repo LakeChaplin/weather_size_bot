e42eaec8fed3931c14c45bb6a27c677ac74a357d
Andrey <a.u.golikov@gmail.com>
2022-12-19 17:09:11 +0300

    add weather script

---
+++ weather.py
@@ -0,0 +1,12 @@
+import requests
+from config import *
+weather_token = get_openweather_token()
+
+def get_weather_for_now():
+    lat = 55.5433
+    lon = 37.5483
+    params = {'units': 'metric',
+            'lang': 'ru'}
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_token}'
+    weather_now = requests.get(url, params=params) 
+    return weather_now.json()
