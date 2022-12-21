fb99eb708774211ee4468eb310a29b630b18556f
Andrey <a.u.golikov@gmail.com>
2022-12-21 20:32:14 +0300

    add group id, db_name

--- config.py
+++ config.py
@@ -8,3 +8,13 @@ def get_openweather_token():
     with open('D:/Projects/Telegram/Tokens/openweathermap-token.txt', 'r') as token:
         weather_token = token.read()
     return weather_token
+
+def get_borov_id():
+    with open('D:/Projects/Telegram/Tokens/borovy_group_id.txt', 'r') as id:
+        group_id = id.read()
+    return group_id
+
+TG_BOT_TOKEN = get_bot_token()
+OPENWEATHER_TOKEN = get_openweather_token()
+DB_FILENAME = 'botuploads.db'
+MY_ID = get_borov_id()
