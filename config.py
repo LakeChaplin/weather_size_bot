5df13cc97f4fb3e2934b4f77fbce66602561d162
Andrey <a.u.golikov@gmail.com>
2023-02-03 00:38:41 +0300

    add auth info for pikabu

--- config.py
+++ config.py
@@ -14,7 +14,19 @@ def get_borov_id():
         group_id = id.read()
     return group_id
 
+def get_login_pikabu():
+    with open('D:/Projects/Telegram/Tokens/pikabu_login_pass.txt') as auth:
+        login = auth.readlines()
+    return login[0].rstrip('\n')
+
+def get_pass_pikabu():
+    with open('D:/Projects/Telegram/Tokens/pikabu_login_pass.txt') as auth:
+        password = auth.readlines()
+    return password[1]
+
 TG_BOT_TOKEN = get_bot_token()
 OPENWEATHER_TOKEN = get_openweather_token()
 DB_FILENAME = 'botuploads.db'
 MY_ID = get_borov_id()
+PIKABU_LOGIN = get_login_pikabu()
+PIKABU_PASS = get_pass_pikabu()
